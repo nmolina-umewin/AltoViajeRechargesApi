984a84e43a48024eb24ada885e5e775481c33960
Nicolas Molina <comodinx@gmail.com>
2019-03-17 19:38:02 +0000

    Fixed bug queries

--- models/serviceTokens.js
+++ models/serviceTokens.js
@@ -8,6 +8,6 @@ module.exports = {
         return `SELECT * FROM service_tokens WHERE id_service = ${idService} AND TIMESTAMPDIFF(HOUR, NOW(), expired_at) > 0`;
     },
     Add         : function(data) {
-        return `INSERT INTO service_token VALUES (NULL, ${data.idService}, "${data.token}", "${data.description}", DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 23 HOUR)), NOW(), NULL)`;
+        return `INSERT INTO service_token VALUES (NULL, ${data.idService}, '${data.token}', '${data.description}', DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 23 HOUR)), NOW(), NULL)`;
     }
 };
