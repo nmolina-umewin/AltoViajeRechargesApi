1ad6a90050f303cafafea20a4fbdb90a6d173ac7
Nicolas Molina <comodinx@gmail.com>
2019-03-17 19:46:54 +0000

    Fixed bug queries

--- utilities/config/database.js
+++ utilities/config/database.js
@@ -4,5 +4,6 @@ module.exports = {
         host : 'localhost',
         user : 'altoviaje',
     password : '05D06455cbfcf7eec1a6d03a18a1ffbe!',
-    database : 'av_recharges'
+    database : 'av_recharges',
+    // debug    : ['ComQueryPacket', 'RowDataPacket']
 };
