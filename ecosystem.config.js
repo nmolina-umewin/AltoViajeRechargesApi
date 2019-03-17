8ba0b5d5a80d4d002329ca003055a3a4a5825cca
Nicolas Molina <comodinx@gmail.com>
2019-03-17 18:16:59 +0000

    Add pm2 file config

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const os = require('os');
+const pkg = require('./package.json');
+
+const APP_MAIN = pkg.main;
+const APP_NAME = pkg.name;
+
+module.exports = {
+    apps : [{
+        name: APP_NAME,
+        script: APP_MAIN,
+        cwd: `/root/altoviaje/${APP_NAME}`,
+        instances: os.cpus().length,
+        exec_mode: 'cluster',
+        out_file: `/root/altoviaje/logs/${APP_NAME}.log`,
+        error_file: `/root/altoviaje/logs/${APP_NAME}.error.log`,
+        env: {
+            NODE_ENV: 'development',
+            watch: 'true',
+            ignore_watch: ['node_modules', 'tests']
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }]
+};
